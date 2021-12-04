572335684a061d6fa1131f4e5e04a85e5fe11433
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-12-04 14:43:56 +0000

    create hello template

--- hello/views.py
+++ hello/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'hello/index.html')
 
 def brian(request):
     return HttpResponse('Hello, Brian!')
